60657c7b1aaa8999f397895d91dbe7e0cf917e48
Gustavo-HA <gustavo_ha40@gmail.com>
2022-11-08 00:09:35 -0600

    putas carpetas

--- mdir.py
+++ mdir.py
@@ -8,4 +8,6 @@ nd = np.round(nd,2)
 
 for i in nd:
     os.system(f'md {i}')
-
+    os.chdir(f'C://Users//pc//Desktop//Proyecto_AFE//rho_v//{i}')
+    os.system('type nul > .gitignore')
+    os.chdir(r'C:\Users\pc\Desktop\Proyecto_AFE\rho_v')
